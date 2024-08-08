c153148613f0f66da40a2c534a382c91e359e8bf
Ebraim Da Costa Sambo <107747841+EbraimSambo@users.noreply.github.com>
2024-08-08 22:15:22 +0000

    init

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getData = void 0;
+const getData = (url) => {
+    return [
+        {
+            name: "  Ebraim Sambo"
+        },
+        {
+            name: "  Ebraim Sambo"
+        }
+    ];
+};
+exports.getData = getData;
+console.log((0, exports.getData)("samb"));
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export const getData =(url: string)=>{
+
+    return[
+        {
+            name:"  Ebraim Sambo"
+        },
+        {
+            name:"  Ebraim Sambo"
+        }
+    ]
+}
+
+console.log(getData("samb"))
